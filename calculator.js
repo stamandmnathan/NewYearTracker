abc47489450cf77f43431023f47344c97a9aeb0b
BraveSword123 <annabelle.gyeniba.moore@gmail.com>
2026-03-29 18:24:53 -0700

    Enhance calculator UI and functionality: update styles, add clear button, and implement calculation logic

---
+++ calculator.js
@@ -0,0 +1,16 @@
+const display = document.getElementById("inputCalculator"); 
+
+function appendToDisplay(value){
+    display.value += value; 
+}
+
+function clearDisplay(){
+    display.value = ""; 
+}
+function calculate(){
+    try{
+    display.value = eval(display.value); 
+    } catch(error){
+        display.value = "Error"; 
+    }
+}
